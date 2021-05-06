1900069ea9d2100cf991db8b171321ef794cbe48
VeronikaDavidova <Veronika.Davidova10@DIXONSCARPHONE.COM>
2021-05-06 15:22:50 +0200

    vd addRemoveElements

--- pages/addRemoveElementsPage.js
+++ pages/addRemoveElementsPage.js
@@ -1,15 +1,19 @@
 const { I } = inject();
 
 class AddRemoveElementsPage {
-    buttons = {
-        addElement: '//*[@id="content"]/div/button',
-        delete: '//*[@id="elements"]/button'
-      }
+        buttons = {
+            addElement: '//*[@id="content"]/div/button',
+            delete: '//*[@id="elements"]/button'
+          }
+    
+        toggleToAdd() {
+            I.click(this.buttons.addElement),
+            I.waitForEnabled(this.buttons.delete, 10)
 
-    toggleToAdd() {
-        I.click(this.buttons.addElement)
-
-    }
+        }
+        
 }
-module.exports = new AddRemoveElementsPage()
+
+module.exports = new AddRemoveElementsPage();
 module.exports.AddRemoveElementsPage = AddRemoveElementsPage;
+
